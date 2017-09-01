c208355d77ae83236e02f053ea13c8a4fe2f7ebc
Cauan.KMS <cauanv@hotmail.com>
2017-09-01 11:05:18 -0300

    Have A Nice Day, By Bon Jovi
    https://youtu.be/uCg2BoKiuOM
    
    Just one piece of code to have a nice day :)

---
+++ Cauan_K.py
@@ -0,0 +1,41 @@
+fruits = ["goiaba", "pera", "mamao", "abacaxi maneiro", "amora"]
+
+#for fruit in fruits:
+ # print("Fruta: ", fruit)
+  
+#for letter in "Cauan":
+ # print(letter)
+  
+#cont = 0
+
+#while cont < 21:
+#  print("Sla " + str(cont))
+#  cont = cont + 1
+  
+#  print("")
+
+#stringao = input("Wanna get it? \n")
+
+for letra in "DER K PROGRAM":
+  print(letra)
+  
+  
+print("\n \n")
+
+#string2 = input("ARE U READY???? \n")
+
+#if(string2 == "PARARAM PAM, PARARAMPARARAMPARARAMPARAM"):
+#  print("PAM, PAM PAM, PAM PAAAAAM, PARARAMPARARAMPARARAM")
+#else:
+#  print("Affz")
+
+string3 = input("Do u wanna have a nice day? \n")
+
+part_song = ["\nOOOOOOOOOH, just one thing i hang onto","That gets me through the night","IIIIIII aint gonna do what i dont want to","I'm gonna live my life","Shining like a diamond","Rolling with the dice","Standing on the ledge","I'll show the wind how to fly","When the world gets in my faaaaaace, i say: ","HAVE A NICE DAY"]
+
+
+if(string3 == "YES"):
+  for parts in range(len(part_song)):
+    print(part_song[parts])
+  
+  print("\n \n https://www.youtube.com/watch?v=uzWO7O-qHWI")
